e8eb4cb14eb2cc22fc608d95f8467568692c1a5c
Katie Surrence <katie@Katies-Air.home>
2017-09-25 00:32:29 -0400

    rolling back last change

--- server/api/session.js
+++ server/api/session.js
@@ -4,12 +4,10 @@ module.exports = router
 router.get('/end', (req, res, next) => {
   req.session.destroy()
   res.json({ session: '' })
-    .catch(next)
 })
 
 router.get('/', (req, res, next) => {
   res.json({ session: req.sessionID })
-    .catch(next)
 })
 
 
